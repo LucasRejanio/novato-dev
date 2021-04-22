99341adda7d9cdba2ba12c7c9509c5de0486c3d5
Gabriel Gradinar <gabrielgradinar@hotmail.com>
2021-04-22 19:43:06 -0300

    opa, esqueci de adicionar a pasta nova kk

---
+++ modulos/comandos/respostas/java.py
@@ -0,0 +1,18 @@
+JAVA = {
+    "if/else":[
+        "texto explicando o tópico",
+        "exemplo de código"
+    ],
+    "do/while":[
+        "texto explicando o tópico",
+        "exemplo de código"
+    ],
+    "variaveis":[
+        "texto explicando o tópico",
+        "exemplo de código"
+    ],
+    "operador lógico":[
+        "texto explicando o tópico",
+        "exemplo de código"
+    ],
+}
---
+++ modulos/comandos/respostas/python.py
@@ -0,0 +1,18 @@
+PYTHON = {
+    "if/else":[
+        "texto explicando o tópico",
+        "exemplo de código"
+    ],
+    "do/while":[
+        "texto explicando o tópico",
+        "exemplo de código"
+    ],
+    "variaveis":[
+        "texto explicando o tópico",
+        "exemplo de código"
+    ],
+    "operador lógico":[
+        "texto explicando o tópico",
+        "exemplo de código"
+    ],
+}
