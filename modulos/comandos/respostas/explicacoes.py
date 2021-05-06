a50089caa8f4cb767bfa48842e236fbd6d486025
Gabriel Gradinar <gabrielgradinar@hotmail.com>
2021-05-06 20:06:37 -0300

    arquivo com as explicaçoes separado

---
+++ modulos/comandos/respostas/explicacoes.py
@@ -0,0 +1,18 @@
+EXPLICACOES = {
+    "if_else": """
+        A condicional if  é uma estrutura condicional que executa a afirmação,
+        dentro do bloco, se determinada condição for verdadeira.
+        Se for falsa, executa as afirmações dentro de else.
+    """,
+    "hello_world":"""
+        Um "Olá , mundo !" geralmente é um programa de computador que emite ou exibe a mensagem " Hello , World !". 
+    Esse programa é muito simples na maioria das linguagens de programação e costuma ser usado para ilustrar a sintaxe básica de uma linguagem de programação. 
+    Freqüentemente, é o primeiro programa escrito por pessoas que estão aprendendo a codificar. """,
+    "variaveis":"exemplo de código",
+    "operadores_logicos":"exemplo de código",
+    "operadores_relacionais":"exemplo de código",
+    "tipagem":"exemplo de código",
+    "operacoes_matematicas":"exemplo de código",
+    "operador lógico":"exemplo de código",
+
+}
